b708d53be48d546ba095613402dea4d3e984ebe1
Anthony <Anthony@192.168.1.123>
2015-11-05 13:01:08 +0100

    Added $encryption to gegevens.php

--- applications/playground/config/config.php
+++ applications/playground/config/config.php
@@ -1,5 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+include_once "gegevens.php";
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
@@ -224,7 +226,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = $encryption;
 
 /*
 |--------------------------------------------------------------------------
